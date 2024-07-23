d3de46226730adbf3e78463bdab90306af69c13b
Crana <cranavincy@karunya.edu.in>
2024-07-23 07:30:33 +0530

    Hello World with new line

---
+++ HelloWorldNewLine.py
@@ -0,0 +1,20 @@
+# Write a program to print the given sample output.
+
+# Input Format
+
+# NA
+
+# Constraints
+
+# NA
+
+# Output Format
+
+# The output prints the string
+
+# Sample Output 0
+
+# Hello World
+# Hello World
+
+print("Hello World\nHello World")
