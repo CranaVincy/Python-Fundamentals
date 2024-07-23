d306fb53a641bfa29682166193533f3c129015b1
Crana <cranavincy@karunya.edu.in>
2024-07-23 07:30:15 +0530

    print "Hello, world"

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,19 @@
+# Write a program to print "Hello, World!"
+
+# Input Format
+
+# NA
+
+# Constraints
+
+# NA
+
+# Output Format
+
+# Output prints the string
+
+# Sample Output 0
+
+# "Hello, World!"
+
 print("Hello, World")
