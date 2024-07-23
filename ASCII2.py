61c0bec21e7d3e46d29f378763ed90279d795197
Crana <cranavincy@karunya.edu.in>
2024-07-23 07:29:29 +0530

    convert num to ascii

---
+++ ASCII2.py
@@ -0,0 +1,29 @@
+# Write a program to get a number (ASCII value) as input and print its equivalent character.
+
+# Input Format
+
+# Input consists of one integer
+
+# Constraints
+
+# NA
+
+# Output Format
+
+# Output prints the character
+
+# Sample Input 0
+
+# 119
+# Sample Output 0
+
+# w
+# Sample Input 1
+
+# 105
+# Sample Output 1
+
+# i
+
+num = int(input())
+print(chr(num))
