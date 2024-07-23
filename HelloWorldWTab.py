40871e4e5d721fa5c788deafe39082771a5a2d4a
Crana <cranavincy@karunya.edu.in>
2024-07-23 07:30:51 +0530

    Hello World With Tab

--- HelloWorldWTab.py
+++ HelloWorldWTab.py
@@ -1,1 +1,20 @@
+# Write a program to print Hello World with tab
+
+# Input Format
+
+# NA
+
+# Constraints
+
+# NA
+
+# Output Format
+
+# The output prints the string.
+
+# Sample Output 0
+
+# Hello World    Hello World
+
+
 print("Hello World\tHello World")
