7902ab7a5158d0bad82c994ce449c7217b7b5b44
Crana <cranavincy@karunya.edu.in>
2024-07-23 07:27:27 +0530

    ASCII1.py

---
+++ ASCII1.py
@@ -0,0 +1,29 @@
+# Write a program to get a character as an input and print its ASCII value.
+
+# Input Format
+
+# Input consists of one character
+
+# Constraints
+
+# Character should be an alphabet both lowercase & uppercase
+
+# Output Format
+
+# Output prints the ascii value
+
+# Sample Input 0
+
+# a
+# Sample Output 0
+
+# 97
+# Sample Input 1
+
+# A
+# Sample Output 1
+
+# 65
+
+ch = input()
+print(ord(ch))
